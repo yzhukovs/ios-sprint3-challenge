7eb3cc3dc43cc73ce9ba031b283aec07d0d4f400
yzhukovs <yzhukovs@bumnetworks.com>
2018-10-26 13:04:12 -0400

    detail view controller and table view controller

---
+++ Pokemon/Pokemon/ImageLoader.swift
@@ -0,0 +1,9 @@
+//
+//  ImageLoader.swift
+//  Pokemon
+//
+//  Created by Yvette Zhukovsky on 10/26/18.
+//  Copyright © 2018 Yvette Zhukovsky. All rights reserved.
+//
+
+import Foundation
---
+++ Pokemon/Pokemon/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  Pokemon
+//
+//  Created by Yvette Zhukovsky on 10/26/18.
+//  Copyright © 2018 Yvette Zhukovsky. All rights reserved.
+//
+
+import Foundation
